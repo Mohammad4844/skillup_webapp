01298a0d25a59cb543ad43e18d6c4d4b5dfbf7f1
Mohammad <mohammadimaan484@gmail.com>
2023-11-19 09:10:13 -0500

    ml integration almost complete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ApplicationRecord
+  def find_for_user
+    user = current_user
+    article_ids = [user.last_article, user.second_last_article, user.third_last_article]
+
+    matches = [[], [], []]
+
+    best_matches = []
+    matches.each do |match|
+      match = match.map.with_index { |value, i| [value.abs, i] }
+      closest = match.sort_by { |distance, _| distance}.first(5)
+      indices = closest.map { |_, i| i}
+      best_matches += indices
+    end
+    Article.where(best_matches.uniq)
+  end
 end
